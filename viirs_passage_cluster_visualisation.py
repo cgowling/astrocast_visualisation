9bb0082a0844e485eb3df588fd2e412670707d94
cgowling <c.gowling@sussex.ac.uk>
2024-10-30 12:37:36 +0000

    reworking page layout to suit more screen sizes

--- viirs_passage_cluster_visualisation.py
+++ viirs_passage_cluster_visualisation.py
@@ -20,7 +20,7 @@ import matplotlib as mp
 st.set_page_config(
     page_title="PASSAGE Vegetation Condition Dashboard",
     layout="wide",
-    initial_sidebar_state="expanded")
+    initial_sidebar_state="collapsed")
 
 # st.sidebar.success("Select a regio above.")
 
@@ -33,7 +33,7 @@ st.set_page_config(
 # TITLE
 # ________________________________________
 
-colal,  colbe = st.columns([5, 40], vertical_alignment= 'center',  gap='small')
+colal,  colbe = st.columns([10, 40], vertical_alignment= 'center',  gap="medium")
 with colal:
     st.image("PASSAGE_final_logo.png", width=200)
 with colbe:
@@ -41,56 +41,63 @@ with colbe:
 
 st.divider()
 
-col = st.columns((1, 2.5, 2), gap='medium')
+# # :earth_africa: VCI3M, NDVI Monitoring & Forecasting
+'''
+This tool shows historical and forecasted 3-month average vegetation condition index (VCI3M) and the historical Normalized Difference Vegetation Index (NDVI) generated from VIIRS data across PASSAGE's regions of interest.
+PASSAGE focuses on 3 of IGAD's cross boundary clusters  (1:Karamoja, 2:Moyale, 3:Mandera)*. To view relevant data, select a cluster from the drop down menu below and click a sub county on the map to see a VCI3M forecast for that region.  
+
+Note: This tool is under active development. 
+'''
+
+# ________________________________________
+# Sub title forecast VCI3M
+# ________________________________________
+st.header("Forecasted VCI3M", divider='gray')
+
+
+
+
+# ________________________________________
+# Select sub county
+# ________________________________________
+
+DATA_SOURCE = "VIIRS"
+
+# SELECT WHICH CLUSTER
+
+clusters = ['Karamoja', "Moyale", "Mandera"]
+selected_cluster_name = st.selectbox("Select IGAD Cluster", clusters)
+
+# ______________________________________
+# LOAD SELECTED DATA
+# ________________________________________
+
+cluster_labels = {'Karamoja': 'CLUSTER_1',
+                  "Moyale": "CLUSTER_2",
+                  "Mandera": "CLUSTER_3"}
+
+selected_cluster = cluster_labels[selected_cluster_name]
+
+datasets, df, df_NDVI, last_observed_VCI3M, min_date, max_date, dates = uf.load_observed_data(DATA_SOURCE,
+                                                                                              selected_cluster)
+
+df_vci_smoothed, df_vci3m_smoothed = uf.load_smoothed_data(DATA_SOURCE, selected_cluster, datasets)
+
+if selected_cluster == "CLUSTER_1":
+    shapefile_path = "./shapefiles/IGAD_Cluster_123/IGAD_Cluster_1.shp"
+    LEVEL_3_LABEL = "County"
+elif selected_cluster == "CLUSTER_2":
+    shapefile_path = "./shapefiles/IGAD_Cluster_123/IGAD_Cluster_2.shp"
+    LEVEL_3_LABEL = "WOREDANAME"
+else:
+    shapefile_path = "./shapefiles/IGAD_Cluster_123/IGAD_Cluster_3.shp"
+    LEVEL_3_LABEL = "DISTRICT"
+
+st.write("Select a sub county on the map to see VCI3M forecast and historical data for that region  ")
+
+col = st.columns(( 1, 1), gap='medium')
 
 with col[0]:
-    # # :earth_africa: VCI3M, NDVI Monitoring & Forecasting
-    '''
-    This tool shows historical and forecasted 3-month average vegetation condition index (VCI3M) and the historical Normalized Difference Vegetation Index (NDVI) generated from VIIRS data across PASSAGE's regions of interest.
-    PASSAGE focuses on 3 of IGAD's cross boundary clusters  (1:Karamoja, 2:Moyale, 3:Mandera)*. To view relevant data, select a cluster from the drop down menu below and click a sub county on the map to see a VCI3M forecast for that region.  
-    
-    Note: This tool is under active development. 
-    '''
-
-with col[1]:
-    st.header("Latest observed VCI3M  ", divider='gray')
-    st.write("Select sub county to see VCI3M forecast ")
-    # ________________________________________
-    # Select sub county
-    # ________________________________________
-
-    DATA_SOURCE = "VIIRS"
-
-    # SELECT WHICH CLUSTER
-
-    clusters = ['Karamoja', "Moyale", "Mandera"]
-    selected_cluster_name = st.selectbox("IGAD Cluster", clusters)
-
-    # ______________________________________
-    # LOAD SELECTED DATA
-    # ________________________________________
-
-    cluster_labels = {'Karamoja': 'CLUSTER_1',
-                      "Moyale": "CLUSTER_2",
-                      "Mandera": "CLUSTER_3"}
-
-    selected_cluster = cluster_labels[selected_cluster_name]
-
-    datasets, df, df_NDVI, last_observed_VCI3M, min_date, max_date, dates = uf.load_observed_data(DATA_SOURCE,
-                                                                                                  selected_cluster)
-
-    df_vci_smoothed, df_vci3m_smoothed = uf.load_smoothed_data(DATA_SOURCE, selected_cluster, datasets)
-
-    if selected_cluster == "CLUSTER_1":
-        shapefile_path = "./shapefiles/IGAD_Cluster_123/IGAD_Cluster_1.shp"
-        LEVEL_3_LABEL = "County"
-    elif selected_cluster == "CLUSTER_2":
-        shapefile_path = "./shapefiles/IGAD_Cluster_123/IGAD_Cluster_2.shp"
-        LEVEL_3_LABEL = "WOREDANAME"
-    else:
-        shapefile_path = "./shapefiles/IGAD_Cluster_123/IGAD_Cluster_3.shp"
-        LEVEL_3_LABEL = "DISTRICT"
-
     # ________________________________________
     # Create map showing selected sub county
     # ________________________________________
@@ -156,10 +163,8 @@ with col[1]:
     #     st.write(output["last_object_clicked_tooltip"])
 
 
-    # ________________________________________
-    # Sub title forecast VCI3M
-    # ________________________________________
-    st.subheader("Forecasted VCI3M", divider='gray')
+
+with col[1]:
 
     # ________________________________________
     # calculate errors from hindcasts for this subcounty
@@ -181,26 +186,29 @@ with col[1]:
 
     st.pyplot(fig3)
 
-with col[2]:
-    st.header(f"Historical data {selected_coulum} ", divider='gray')
-
-    # # ________________________________________
-    # # Slider to select time period
-    # # ________________________________________
-
-    from_date, to_date = st.slider(
-        'Which time period are you interested in?',
-        min_value= min_date,
-        max_value= max_date,
-        value=[min_date, max_date])
-
-
-    # Filter the data
-    filtered_df = df.loc[from_date:to_date]
-    filtered_df_NDVI = df_NDVI.loc[from_date:to_date]
 
 
 
+st.header(f"Historical data {selected_coulum} ", divider='gray')
+
+# # ________________________________________
+# # Slider to select time period
+# # ________________________________________
+
+from_date, to_date = st.slider(
+    'Which time period are you interested in?',
+    min_value=min_date,
+    max_value=max_date,
+    value=[min_date, max_date])
+
+# Filter the data
+filtered_df = df.loc[from_date:to_date]
+filtered_df_NDVI = df_NDVI.loc[from_date:to_date]
+
+
+
+col_historical = st.columns(( 1, 1), gap='medium')
+with col_historical[0]:
     # # ________________________________________
     # # Create figures showing historical VCI3M
     # # ________________________________________
@@ -219,6 +227,8 @@ with col[2]:
 
         st.dataframe(filtered_df)
 
+with col_historical[1]:
+
     # # ________________________________________
     # # Display historical NDVI
     # # ________________________________________
@@ -233,6 +243,10 @@ with col[2]:
         # st.subheader("Historical weekly NDVI all regions ", divider='gray')
         # streamlit line chart
         st.dataframe(filtered_df_NDVI)
+
+
+
+
 
 #
 #
